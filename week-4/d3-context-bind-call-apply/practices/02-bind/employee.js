1bf1d991c1027bca06a148fd9899aa542ff07363
kenandcrys <kenandcrys@live.com>
2022-12-14 15:37:40 -0600

    day 3

--- week-4/d3-context-bind-call-apply/practices/02-bind/employee.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/employee.js
@@ -1,1 +1,24 @@
 // Your code here
+class Employee {
+    constructor ( name, occupation ){
+        this.name = name;
+        this.occupation = occupation;
+    }
+    sayName(){
+        console.log(`${this.name} says hello.`)
+    }
+    sayOccupation () {
+        console.log(`${this.name} is a ${this.occupation}.`)
+    }
+}
+const john = new Employee(`John Wick`, `Dog Lo
+lover`)
+
+ setTimeout(john.sayName.bind(john), 2000);
+
+setTimeout( john.sayOccupation.bind(john), 5000)
+
+
+
+
+module.exports = Employee;
--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,27 @@
 // Your code here
+
+const chai = require("chai");
+const expect = chai.expect;
+const Employee = require(`./employee`);
+
+
+describe("Employee class", () => {
+    let john;
+     john = new Employee(`John Wick`,`Dog Lover`)
+    
+    describe("Employee constructor", () => {
+        it("should set the name property", () => {
+            expect(john).to.have.property("name");
+            expect(john.name).to.eql("John Wick");
+        });
+
+        it("should set the occupation property", () => {
+            expect(john).to.have.property("occupation");
+            expect(john.occupation).to.eql("Dog Lover");
+        });
+
+
+
+    });
+
+});
